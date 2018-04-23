88ce35fe6d1cc8e916511f429f5b38dd449e7a38
Ahmad <aabda2000@gmail.com>
2018-04-23 23:10:00 +0200

    add htaccess

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@
     public static function getInstance() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=mvc', 'root', 'orange', $pdo_options);
+        self::$instance = new PDO('mysql:host=localhost;dbname=mvc', 'root', 'orange', NULL);
       }
       return self::$instance;
     }
